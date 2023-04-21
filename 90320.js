3ef70323f108c4f38081346caa4a08b7019b0097
joshsj89 <joshsj89@gmail.com>
2023-04-21 02:10:40 -0700

    Initial commit

---
+++ 90320.js
@@ -0,0 +1,48 @@
+const script = document.createElement('script');
+script.src = 'https://unpkg.com/axios/dist/axios.min.js';
+document.head.appendChild(script);
+
+const wrapper = document.getElementById('238596');
+
+// Create a new button element
+const newButton = document.createElement('button');
+newButton.textContent = 'Download';
+
+// Append the new button as a child of the wrapper element, before the existing button
+wrapper.insertBefore(newButton, wrapper.firstChild);
+
+const get_module = async (data_module_url) => {
+
+    //const response = await axios.get(`https://camino.instructure.com/api/v1${data_module_url}/items`);
+    const response = await axios.get(`https://camino.instructure.com/api/v1/courses/90320/modules/${data_module_url}/items`);
+    const data = response.data;
+    return data;
+}
+
+// Downloads separate pdfs
+const get_pdfs = async (data_module_url) => {
+    const data = await get_module(data_module_url);
+
+    for (const info of data) {
+        const url = info['url'];
+        const response = await axios.get(url);
+        const data2 = response.data;
+        const filename = data2['display_name'];
+        const url2 = data2['url'];
+
+        if (data2['content-type'] === 'application/pdf') {
+            const response2 = await axios.get(url2, { responseType: 'arraybuffer' });
+            const blob = new Blob([response2.data]);
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+            link.click();
+            console.log(`"${data2['display_name']}" downloaded successfully.`);
+        }
+    }
+}
+
+// Listen for click on button
+newButton.addEventListener('click', () => {
+    get_pdfs(wrapper.id);
+});
